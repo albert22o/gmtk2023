e9e1b78d22bc17d9dbbab308d0c783312097cb32
AndrewD <67038546+AndrewDilman@users.noreply.github.com>
2023-07-08 14:49:20 +0700

    fпередвжение юнитов по тайлам

---
+++ scripts/unit.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+onready var navig = $NavigationAgent2D
+export var speed = 100
+
+func _ready():
+	Find_paht()
+	pass
+
+func _physics_process(delta):
+	Walk_to_target()
+	pass
+
+
+func Find_paht():
+	var mas = get_tree().get_nodes_in_group("exit")
+	var target = mas[0]
+	navig.set_target_location(target.global_position)
+
+
+func Walk_to_target():
+	var direction = global_position.direction_to(navig.get_next_location())
+	var velocity = direction*speed
+	move_and_slide(velocity)
+#	navig.set_velocity(velocity)
+#	$Line2D.points = navig.get_nav_path()
+	pass
+
+
+func _on_NavigationAgent2D_path_changed():
+	pass
